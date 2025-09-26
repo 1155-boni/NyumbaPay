aefdcec839eaea7c757e791290e076e39bda80c1
Boniface Masila <dietboni@gmail.com>
2025-09-27 00:55:10 +0300

    Add User model

---
+++ rentals/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('landlord', 'Landlord'),
+        ('tenant', 'Tenant'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='tenant')
